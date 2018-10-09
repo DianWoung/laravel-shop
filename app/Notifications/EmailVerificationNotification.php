f91775fa6357de275d6b1349a47fbd85ad8b7409
dianwang <dian126@foxmail.com>
2018-10-09 15:32:04 +0800

    触发激活邮件

--- app/Notifications/EmailVerificationNotification.php
+++ app/Notifications/EmailVerificationNotification.php
@@ -4,8 +4,9 @@ namespace App\Notifications;
 
 use Illuminate\Bus\Queueable;
 use Illuminate\Notifications\Notification;
-use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
+use Cache;
+use Illuminate\Support\Str;
 
 class EmailVerificationNotification extends Notification
 {
@@ -40,11 +41,15 @@ class EmailVerificationNotification extends Notification
      */
     public function toMail($notifiable)
     {
+        $token = Str::random(16);
+
+        Cache::set('email_verification_'.$notifiable->email, $token, 30);
+        $url = route('email_verification.verify', ['email' => $notifiable->email, 'token' => $token]);
         return (new MailMessage)
                     ->greeting($notifiable->name.'您好:')
                     ->subject('注册成功，请验证您的邮箱')
                     ->line('请点击下方链接验证您的邮箱')
-                    ->action('Notification Action', url('/'));
+                    ->action('验证', $url);
 
     }
 
--- routes/web.php
+++ routes/web.php
@@ -16,10 +16,10 @@ Route::get('/', 'PagesController@root')->name('root');
 Auth::routes();
 
 Route::group(['middleware' => 'auth'], function (){
+    Route::get('/email_verification/send', 'EmailVerificationController@send')->name('email_verification.send');
     Route::get('/email_verify_notice', 'PagesController@emailVerifyNotice')->name('email_verify_notice');
+    Route::get('/email_verification/verify', 'EmailVerificationController@verify')->name('email_verification.verify');
     Route::group(['middleware' => 'email_verified'], function (){
-       Route::get('/test', function (){
-         return "Your email is verified";
-       });
+
     });
 });
