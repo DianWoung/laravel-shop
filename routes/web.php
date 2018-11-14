5dca2bc66d87b014e44718b988bad180978cc601
dianwang <dian126@foxmail.com>
2018-11-14 20:02:15 +0800

    收货地址

--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,6 @@ Route::group(['middleware' => 'auth'], function (){
     Route::get('/email_verify_notice', 'PagesController@emailVerifyNotice')->name('email_verify_notice');
     Route::get('/email_verification/verify', 'EmailVerificationController@verify')->name('email_verification.verify');
     Route::group(['middleware' => 'email_verified'], function (){
-
+        Route::get('user_addresses', 'UserAddressesController@index')->name('user_addresses.index');
     });
 });
