3e03b176df4ff64a896d47493ab1e8fe5672d104
DianWoung <dian126@foxmail.com>
2019-03-20 09:04:51 +0800

    商品列表fix

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -3,15 +3,15 @@
 
 @section('content')
     <div class="row">
-        <div class="col-lg-10 offset-lg-1">
-            <div class="card">
-                <div class="card-body">
+        <div class="col-lg-10 col-lg-offset-1">
+            <div class="panel panel-default">
+                <div class="panel-body">
                     <div class="row products-list">
                         @foreach($products as $product)
-                            <div class="col-3 product-item">
+                            <div class="col-xs-3 product-item">
                                 <div class="product-content">
                                     <div class="top">
-                                        <div class="img"><img src="{{ $product->image_url }}" alt="" width="100px"></div>
+                                        <div class="img"><img src="{{ $product->image_url }}" alt=""></div>
                                         <div class="price"><b>￥</b>{{ $product->price }}</div>
                                         <div class="title">{{ $product->title }}</div>
                                     </div>
@@ -23,7 +23,7 @@
                             </div>
                         @endforeach
                     </div>
-                    <div class="float-right">{{ $products->render() }}</div>
+                    <div class="pull-right">{{ $products->render() }}</div>
                 </div>
             </div>
         </div>
