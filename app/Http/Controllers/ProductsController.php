4bd416b6d4c1d5b67e7ff202675df1f2b8366b9a
DianWoung <dian126@foxmail.com>
2019-03-25 08:14:29 +0800

    商品列表-商品展示

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Exceptions\InvalidRequestException;
 use App\Models\Product;
 use Illuminate\Http\Request;
 
@@ -48,5 +49,14 @@ class ProductsController extends Controller
                 'order'  => $order,
             ],
         ]);
+    }
+
+    public function show(Product $product, Request $request)
+    {
+        if (!$product->on_sale) {
+            throw new InvalidRequestException('商品未上架');
+        }
+
+        return view('products.show', ['product' => $product]);
     }
 }
