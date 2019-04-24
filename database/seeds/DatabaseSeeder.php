1fd82853e2924ffc12f0a5f35f4ffaab7ce951f6
dianwang <dian126@foxmail.com>
2019-04-24 14:16:06 +0800

    购物车页面

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,7 +13,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         //填充laravel-admin数据
-        //$this->call(AdminTablesSeeder::class);
+        $this->call(AdminTablesSeeder::class);
         // $this->call(UsersTableSeeder::class);
     }
 }
